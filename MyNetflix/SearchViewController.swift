66d10037e6b01a2b52bd913d075dfa8ee29743f0
manggong <jihwank1995@gmail.com>
2021-01-26 19:46:33 +0900

    add SearchBar, keyboard toggle func in SearchViewController

--- MyNetflix/SearchViewController.swift
+++ MyNetflix/SearchViewController.swift
@@ -10,8 +10,27 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var resultCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    
+    private func dismissKeyboard() {
+        searchBar.resignFirstResponder()
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        dismissKeyboard()
+        
+        guard let searchTerm = searchBar.text, searchTerm.isEmpty == false else {return}
+        
+        print("---->\(searchBar)")
+    }
 }
